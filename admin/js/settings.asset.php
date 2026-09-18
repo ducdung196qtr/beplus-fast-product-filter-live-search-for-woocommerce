06372a85b7d26813e24f26f76e7579faf9e9f845
root <root@linux7703.cloudnest.vn>
2026-09-18 16:24:50 +0000

    build: refresh release asset manifests
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

--- admin/js/settings.asset.php
+++ admin/js/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return [ 'dependencies' => [ 'jquery', 'wp-color-picker' ], 'version' => '1789747857841' ];
+<?php return [ 'dependencies' => [ 'jquery', 'wp-color-picker' ], 'version' => '1789748673715' ];
