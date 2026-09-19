caed1b4dcb8335415107eba78f963905ccbe0a4a
root <root@linux7703.cloudnest.vn>
2026-09-19 05:27:11 +0000

    build: align block metadata with 1.2.5
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

--- admin/js/settings.asset.php
+++ admin/js/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return [ 'dependencies' => [ 'jquery', 'wp-color-picker' ], 'version' => '1789795511669' ];
+<?php return [ 'dependencies' => [ 'jquery', 'wp-color-picker' ], 'version' => '1789795629972' ];
